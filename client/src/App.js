386a6b2e3b7d2313c5d164f560b15d3a5e64af5c
youbaidjis <youba.etud@gmail.com>
2022-11-10 10:26:51 -0800

    first try

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React from 'react';
+import actionCable from 'actioncable';
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      messages: []
+    }
+    this.cable = actionCable.createConsumer('/cable')
+  }
+ 
+  componentDidMount() {
+    this.fetchMessages()
+    this.createSubscription()
+  };
+
+  fetchMessages = () => {
+    fetch('/messages')
+      .then(res => res.json())
+      .then(messages => this.setState({ messages: messages }));
+  }
+
+  createSubscription = () => {
+    this.cable.subscriptions.create(
+      { channel: 'MessagesChannel' },
+      { received: message => this.handleReceivedMessage(message) }
+    )
+  }
+
+  mapMessages = () => {
+    return this.state.messages.map((message, i) => 
+      <li key={i}>{message.content}</li>)
+  }
+
+  handleReceivedMessage = message => {
+    this.setState({ messages: [...this.state.messages, message] })
+  }
+
+  handleMessageSubmit = e => {
+    e.preventDefault();
+    const messageObj = {
+      message: {
+        content: e.target.message.value
+      }
+    }
+    const fetchObj = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(messageObj)
+    }
+    fetch('/messages', fetchObj)
+    e.target.reset()
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        {/* <actionCable 
+          channel={{ channel: 'MessagesChannel' }}
+          onReceived={this.handleReceivedMessages}
+        /> */}
+        <h2>Messages</h2>
+        <ul>{this.mapMessages()}</ul>
+        <form>
+          <input name='message' type='text' />
+          <input type='submit' value='Send message' />
+        </form>
+      </div>
+    );
+  }
 }
-
 export default App;
